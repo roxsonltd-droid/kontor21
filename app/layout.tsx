15b039efda1e4184a44bac8bd7c3ee9de9bdf254
Krasimir <lukezester@gmail.com>
2026-08-02 09:24:47 +0300

    fix: force dark mode globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-zinc-950 text-white">
         <LanguageProvider>
           {children}
         </LanguageProvider>
